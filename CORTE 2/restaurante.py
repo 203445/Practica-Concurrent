5f97d3259c5c2b0a6b5cef7bb12ff87cb92e8835
203445 <203445@ids.upchiapas.edu.mx>
2022-11-03 12:54:31 -0600

    modificación del nombre

--- CORTE 2/restaurante.py
+++ CORTE 2/restaurante.py
@@ -1,5 +1,5 @@
-# Karla Maricruz Ruiz Diaz
-# Janeth Alejandra Morales Mendoza
+# Karla Maricruz Ruiz Diaz  	203440
+# Janeth Alejandra Morales Mendoza  203445
 
 from threading import Thread 
 import queue, time
@@ -8,11 +8,7 @@ import random
 
 #clientes llegando 
 bufferRest = queue.Queue(maxsize=20)
-#print(cantrecep.maxsize)
 
-#cantidad de meseros
-
-#print(pormeseros)
 #caoacidad del rest
 rest = queue.Queue(maxsize=20)
 restaurante = queue.Queue(maxsize=20)
@@ -38,68 +34,66 @@ class Persona(threading.Thread):
         #cantidad de recepciones validas
         por = float(20)
         reception = int(por * bufferRest.maxsize/100)
-        #with self.monitor:
-        if not bufferRest.full():
-            for x in range(reception):
-                bufferRest.put(self.id)
-                print("Hay una reservacion del cliente"+ str(self.id+1))
-                self.atendiendo()
-                self.monitor.wait()
-        else:
-            self.atendiendo()
-                # ciclo = False
-
-    def espera(self):
-        ciclo = True
         with self.monitor:
-            #while not bufferOrdenes.empty():
-                # if not bufferOrdenes.empty():
-                    if restaurante.full():
-                        #bufferOrdenes.get(self.id)
-                        print("Cliente esperando: "+str(self.id+1))
-                        time.sleep(3)
-                       # bufferCook.put(self.id)
-                        self.monitor.wait()
-                        #self.entregar()
-                        if not restaurante.empty():
-                            self.ingresar()
-                    else:
-                        self.ingresar()   
-                # else:
-                #     self.atendiendo()
-
+            if not bufferRest.full():
+                for x in range(reception):
+                    self.monitor.notify()
+                    bufferRest.put(self.id)
+                    print("Hay una reservacion del cliente"+ str(self.id+1))
+                    self.monitor.wait()
+                    self.atendiendo()
+                    
+            else:
+                self.atendiendo()
+                
+        time.sleep(5)
+    def espera(self):
+        with self.monitor:
+            if restaurante.full():
+                print("Cliente esperando: "+str(self.id+1))
+                # time.sleep(5)
+                self.monitor.notify()
+                bufferCook.put(self.id)
+                self.monitor.wait()
+                if not restaurante.empty():
+                    self.ingresar()
+            else:
+                self.ingresar()   
+               
+        time.sleep(5)
 
     def entregar(self):
         with self.monitor:
             while not bufferCook.empty():
-                # if not bufferOrdenes.empty():
-                    #if not bufferCook.empty():
-                        bufferCook.get(self.id)
-                        print("Mesero entrega la comida al cliente: "+str(self.id+1))
-                        restaurante.put(self.id)
-                        self.comiendo()
-                        self.monitor.wait()
+                bufferCook.get(self.id)
+                self.monitor.notify()
+                print(" ✅ Mesero entrega la comida al cliente: "+str(self.id+1))
+                restaurante.put(self.id)
+                self.comiendo()
+           
 
     def comiendo(self):
         with self.monitor:
             if not bufferCook.full():
-                print("cliente: "+str(self.id+1)+" Esta Comiendo")
-                time.sleep(random.randint(2, 6))
-                print("cliente: "+str(self.id+1)+" Termino de Comer")
-                restaurante.get(self.id)
+                print("Cliente: "+str(self.id+1)+" Esta Comiendo 🍴")
+                time.sleep(random.randint(5, 9))
                 self.monitor.wait()
+                print("Cliente: "+str(self.id+1)+" Termino de Comer 😁")
+                restaurante.get(self.id)
+                self.ingresar()
+                self.monitor.notify()
             else:
               self.espera()  
 
     
     def cocinar (self):
-        ciclo = True
         with self.monitor:
             while not bufferOrdenes.empty():
                 # if not bufferOrdenes.empty():
                     if not bufferCook.full():
                         bufferOrdenes.get(self.id)
-                        print("Cocinero prepara la comida del cliente: "+str(self.id+1))
+                        self.monitor.notify()
+                        print("🧑‍🍳 Cocinero prepara la comida del cliente: "+str(self.id+1))
                         time.sleep(4)
                         bufferCook.put(self.id)
                         self.monitor.wait()
@@ -107,75 +101,63 @@ class Persona(threading.Thread):
                     else:
                         self.ordenar()
                         self.espera()   
-                # else:
-                #     self.atendiendo()
-
+        time.sleep(4)
 
     def ordenar(self):
         cant = float(10)
         mesero = int(cant * rest.maxsize/100)
         meseros = queue.Queue(maxsize=mesero)
-        ciclo = True
         with self.monitor:
             while not rest.empty():
-                # if not rest.empty():
                     if not bufferOrdenes.full():
                         rest.get(self.id)
+                        self.monitor.notify()
                         for x in range(meseros.maxsize):
-                            print("Mesero Tomando Orden del Cliente: "+str(self.id+1))
+                            print("🧑‍💼 Mesero Tomando Orden del Cliente: "+str(self.id+1))
                             time.sleep(5)
                             bufferOrdenes.put(self.id)
-                            self.monitor.notify()
+                            self.monitor.wait()
                             self.cocinar()
                     else:
                         self.cocinar() 
-                        # self.monitor.wait()
-                # else:
-                #     self.atendiendo()
-                    # self.monitor.wait()
-                    
+                        self.monitor.wait()
+
     def atendiendo(self):
         
-        #with self.monitor:
-        while  not bufferRest.empty():
-            # if not bufferRest.empty():
-            if not rest.full():     
-                bufferRest.get(self.id)
-                print("Atendiendo Cliente:" + str(self.id+1))
-                time.sleep(5)
-                rest.put(self.id)
-                print("Cliente: " +str(self.id+1)+ " ingreso al restaurante")
-                time.sleep(5)
-                self.ordenar()
-                self.monitor.notify()
+        with self.monitor:
+            while  not bufferRest.empty():
+                if not rest.full():     
+                    bufferRest.get(self.id)
+                    print("Atendiendo Cliente:" + str(self.id+1))
+                    self.monitor.notify()
+                    # time.sleep(5)
+                    rest.put(self.id)
+                    self.monitor.wait()
+                    print("Cliente: " +str(self.id+1)+ " ingreso al restaurante")
+                    # time.sleep(5)
                     
-            else:
-                self.reserva()
-                self.ordenar()
-                        # self.monitor.wait()    
-                # else:
-                #     self.ingresar()
-                #     # self.monitor.wait()
+                    self.ordenar()
+                else:
+                    self.reserva()
+                    self.ordenar()
             
     
     def ingresar(self):
-        ciclo = True
-        #with self.monitor:
-            #while not bufferRest.full():
-        if not bufferRest.full():
-            bufferRest.put(self.id)
-            print("Hay un nuevo Cliente: " + str(self.id+1))
-            self.atendiendo()
-            self.reserva()
-                # self.monitor.wait()
-                # if      
-        else:
-            #     self.atendiendo()
-            self.espera()
-            self.reserva()
-            # self.monitor.notify()
+        with self.monitor:
+            if not bufferRest.full():
+                self.monitor.notify()
+                bufferRest.put(self.id)
+                print("Hay un nuevo Cliente: " + str(self.id+1))
+                self.monitor.wait()
+                self.atendiendo()
+                self.reserva()
+                    
+            else:
+                self.espera()
+                self.reserva()
 
     def run(self):
+        
         self.ingresar()
         self.reserva()
         self.atendiendo()
@@ -184,8 +166,6 @@ class Persona(threading.Thread):
         self.entregar()
 
 items_monit = threading.Condition()           
-#person = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-recepcionista = [1]   
 def main():
     personas = []
 
